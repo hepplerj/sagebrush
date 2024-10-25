dacba81e2ea846340e1c519806ae35e58f2c023b
Jason Heppler <jason.heppler@gmail.com>
2024-10-25 14:30:58 -0500

    feat: Add API endpoints for network and map resources

--- config/urls.py
+++ config/urls.py
@@ -5,23 +5,30 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
+
+from network.views import NetworkViewSet, MapViewSet
+
+router = DefaultRouter()
+router.register(r'network', NetworkViewSet, basename='network')
+router.register(r'map', MapViewSet, basename='map')
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path('api/', include(router.urls)),
     path(
         "about/",
         TemplateView.as_view(template_name="pages/about.html"),
         name="about",
     ),
+    path("network/", TemplateView.as_view(template_name="pages/network.html"), name="network"),
+    path("map/", TemplateView.as_view(template_name="pages/map.html"), name="map"),
     # Django Admin, use {% url 'admin:index' %}
     path("taggit/", include("taggit_selectize.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("sagebrush.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
-    # ...
-    # Media files
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
